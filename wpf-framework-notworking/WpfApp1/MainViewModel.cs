e688bd2c976d5f82f5380ac416657be0802fda50
James Montemagno <james.montemagno@gmail.com>
2022-03-15 11:20:11 -0700

    Add samples for wpf-framework

---
+++ wpf-framework-notworking/WpfApp1/MainViewModel.cs
@@ -0,0 +1,34 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace WpfApp1;
+
+public partial class MainViewModel : ObservableObject
+{
+    public MainViewModel()
+    {
+    }
+
+    [ObservableProperty]
+    [AlsoNotifyChangeFor(nameof(FullName))]
+    string firstName;
+
+
+    [ObservableProperty]
+    [AlsoNotifyChangeFor(nameof(FullName))]
+    string lastName;
+
+
+    public string FullName => $"{FirstName} {LastName}";
+
+
+    [ICommand]
+    async Task Submit()
+    {
+        Debug.WriteLine("DEBUG INFO: Submitted");
+        this.FirstName = "test";
+    }
+
+}
---
+++ wpf-framework-notworking/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainViewModel();
+        }
+    }
+}
