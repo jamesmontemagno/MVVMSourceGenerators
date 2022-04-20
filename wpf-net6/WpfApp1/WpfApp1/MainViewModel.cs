0949d017ef55298cf580d2787eaaa9d98671f529
James Montemagno <james.montemagno@gmail.com>
2022-04-20 15:19:51 -0700

    add .net 6 wpf

---
+++ wpf-net6/WpfApp1/WpfApp1/MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Diagnostics;
+
+namespace WpfApp1;
+
+public partial class MainViewModel : ObservableObject
+{
+    [ObservableProperty]
+    [AlsoNotifyChangeFor(nameof(FullName))]
+    string _firstName = "";
+
+    [ObservableProperty]
+    [AlsoNotifyChangeFor(nameof(FullName))]
+    string lastName = "";
+
+    public string FullName => $"{FirstName} {LastName}";
+
+    [ICommand]
+    void Submit()
+    {
+        Debug.WriteLine("DEBUG: Submit entered");
+        FirstName = string.Empty;
+        LastName = string.Empty;
+    }
+}
