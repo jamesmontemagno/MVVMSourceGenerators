aee041b11178c1a532623498563d7c8ad677de6a
James Montemagno <james.montemagno@gmail.com>
2022-05-04 11:09:57 -0700

    Add Xamarin.forms Sample

---
+++ xamarin-forms/App1/App1/App1/MainViewModel.cs
@@ -0,0 +1,68 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Diagnostics;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System;
+
+namespace App1;
+
+[INotifyPropertyChanged]
+public partial class MainViewModel
+
+{
+    int count = 0;
+    public MainViewModel()
+    {
+    }
+
+    [ObservableProperty]
+    [AlsoNotifyChangeFor(nameof(FullName))]
+    string firstName;
+
+    [ObservableProperty]
+    [AlsoNotifyChangeFor(nameof(FullName))]
+    string lastName;
+
+
+
+    public string FullName =>
+        $"{FirstName} {LastName}";
+
+
+    [ICommand]
+    async Task Submit()
+    {
+        Debug.WriteLine("DEBUG INFO: Submitted");
+
+        try
+        {
+            count++;
+            if (count == 1)
+                SubmitInfo(null, 5, null);
+            else if (count == 2)
+                SubmitInfo(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 }, 5, null);
+            else if (count == 3)
+                SubmitInfo(new[] { 1, 2, 3 }, 5, null);
+            else if (count == 4)
+                SubmitInfo(new[] { 1, 2, 3 }, 1, null);
+            else if (count == 5)
+                SubmitInfo(new[] { 1, 2, 3 }, 1, "Hello World");
+        }
+        catch (Exception ex)
+        {
+            await App.Current.MainPage.DisplayAlert("Error", ex.Message, "OK");
+        }
+    }
+
+    public void SubmitInfo(int[] array, int index, string text)
+    {
+        Guard.IsNotNull(array);
+        Guard.HasSizeLessThan(array, 10);
+        Guard.IsInRangeFor(index, array);
+        Guard.IsNotNullOrEmpty(text);
+
+    }
+
+}
